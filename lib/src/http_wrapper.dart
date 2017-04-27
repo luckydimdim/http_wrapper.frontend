4b1cd29b1812676ef835bd0bc3cf706efca0c828
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-04-27 10:42:18 +0300

    CMAS-376 Доработать аутентификацию на frontend'e

--- lib/src/http_wrapper.dart
+++ lib/src/http_wrapper.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:http/http.dart';
 import 'exceptions/unauthorized_error.dart';
 import 'package:auth/auth_service.dart';
@@ -7,10 +8,55 @@ class HttpWrapper {
   final Client _http;
   final AuthenticationService _authenticationService;
 
-  HttpWrapper(this._http, this._authenticationService) {}
+  HttpWrapper(this._http, this._authenticationService);
 
   Future<Response> get(url,
       {Map<String, String> headers, useAuth: true}) async {
+
+    _setAuth(useAuth, headers);
+
+    var response = await _http.get(url, headers: headers);
+
+    _checkResponse(response);
+
+    return response;
+  }
+
+  Future<Response> post(url, {Map<String, String> headers, body,
+    Encoding encoding, useAuth: true}) async {
+
+    _setAuth(useAuth, headers);
+
+    var response = await _http.post(url, headers: headers, body: body, encoding: encoding);
+
+    _checkResponse(response);
+
+    return response;
+  }
+
+  Future<Response> put(url, {Map<String, String> headers, body,
+    Encoding encoding, useAuth: true}) async {
+
+    _setAuth(useAuth, headers);
+
+    var response = await _http.put(url, headers: headers, body: body, encoding: encoding);
+
+    _checkResponse(response);
+
+    return response;
+  }
+
+  Future<Response> delete(url, {Map<String, String> headers, useAuth: true}) async{
+    _setAuth(useAuth, headers);
+
+    var response = await _http.delete(url, headers: headers);
+
+    _checkResponse(response);
+
+    return response;
+  }
+
+  void _setAuth(bool useAuth, Map<String, String> headers) {
     if (useAuth) {
       if (!_authenticationService.isAuth()) {
         throw new UnauthorizedError();
@@ -18,11 +64,12 @@ class HttpWrapper {
 
       headers["Authorization"] = _authenticationService.getToken();
     }
+  }
 
-    var response = await _http.get(url, headers: headers);
-
+  void _checkResponse(Response response) {
     if (response.statusCode == 200) {
-      return response;
+      // OK
+      return;
     } else if (response.statusCode == 401) {
       // Unauthorized
       throw new UnauthorizedError();
@@ -30,4 +77,6 @@ class HttpWrapper {
       throw new Exception('Unknown HTTP error');
     }
   }
+
+
 }
